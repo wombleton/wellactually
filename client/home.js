ed2fae8ad4324daf898e9861b597b9a4eccb5dee
Rowan Crawford <wombleton@gmail.com>
2014-02-04 13:29:52 +1300

    Show factoids on home page

--- client/home.js
+++ client/home.js
@@ -1,2 +1,8 @@
-Template.home.events({
-});
+/* global Meteor */
+Meteor.subscribeWithPagination('factoids', 10);
+
+Template.home.factoids = function() {
+  return Factoids.find({}, { $sort: {
+    createdAt: -1
+  }});
+};
